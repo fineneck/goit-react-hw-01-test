7b783a863321790cd223624088d26c4fd70a204d
OleksiiMikhieiev <babybong0993@gmail.com>
2024-06-23 03:52:16 +0300

    examples

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,41 +1,57 @@
-// import { FaBeer } from "react-icons/fa";
-// import AppBar from "../AppBar/";
-// import officers from "../../officers.json";
-// import OfficerList from "../OfficerList/OfficerList";
-import ProfileSection from "../ProfileSection";
+import { FaBeer } from "react-icons/fa";
+import AppBar from "../AppBar/AppBar";
+import officers from "../../officers.json";
+import OfficerList from "../OfficerList/OfficerList";
 import css from "./App.module.css";
+// import ProfileSection from "../ProfileSection/ProfileSection";
 // import { Children } from "react";
+
+// const UserInfo = () => {
+//   return <div>Олексій Міхєєв</div>;
+// };
+
+// const Points = () => {
+//   return (
+//     <ul>
+//       <li>
+//         У GoIT ми дбаємо про освітній досвід наших студентів, і хочемо, щоб вам
+//         було цікаво та весело вчитися.
+//       </li>
+//     </ul>
+//   );
+// };
+
+// const Levels = () => {
+//   return (
+//     <ul>
+//       <li>Виконуй домашні завдання та автоперевірки</li>
+//     </ul>
+//   );
+// };
 
 export default function App() {
   return (
     <div className={css.container}>
-      <ProfileSection title="Рейтинг">
-        <div>Олексій Міхєєв</div>
+      {/* <ProfileSection title="Рейтинг">
+        <UserInfo />
       </ProfileSection>
       <ProfileSection
         title="Що це за очки досвіду та рівні?
 "
       >
-        <ul>
-          <li>
-            У GoIT ми дбаємо про освітній досвід наших студентів, і хочемо, щоб
-            вам було цікаво та весело вчитися.
-          </li>
-        </ul>
+        <Points />
       </ProfileSection>
       <ProfileSection
         title="Як отримати більше очків досвіду?
 "
       >
-        <ul>
-          <li>Виконуй домашні завдання та автоперевірки</li>
-        </ul>
-      </ProfileSection>
-      {/* <AppBar />
+        <Levels />
+      </ProfileSection> */}
+      <AppBar />
       <h1 className={css.title}>
         ~Dream Team~ <FaBeer className={css.icon} />
       </h1>
-      <OfficerList items={officers} /> */}
+      <OfficerList items={officers} />
     </div>
   );
 }
